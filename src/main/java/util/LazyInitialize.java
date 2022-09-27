51c6bd90fced49147a321889452e7cc5ea1198d7
Michael Pusl <pusl03@ads.uni-passau.de>
2022-09-27 20:20:32 +0200

    Added: Class LazyInitialize.

---
+++ src/main/java/util/LazyInitialize.java
@@ -0,0 +1,34 @@
+package util;
+
+import java.util.function.Supplier;
+
+import static java.util.Objects.requireNonNull;
+
+
+/**
+ * This class can be used to lazily initialize a variable in a thread-safe
+ * manner.
+ *
+ * @param <T> The type of the variable to be initialized.
+ */
+public final class LazyInitialize<T> {
+    private final Supplier<T> constructor;
+    // See: https://www.cs.umd.edu/~pugh/java/memoryModel/DoubleCheckedLocking.html
+    private volatile T instance = null;
+
+    public LazyInitialize(final Supplier<T> constructor) {
+        this.constructor = requireNonNull(constructor);
+    }
+
+    public T get() {
+        if (instance == null) {
+            synchronized (LazyInitialize.class) {
+                if (instance == null) {
+                    instance = constructor.get();
+                }
+            }
+        }
+
+        return instance;
+    }
+}
